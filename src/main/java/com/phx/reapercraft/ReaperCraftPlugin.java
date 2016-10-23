2a1e752ea1884e46cb54c1040bab079f53e020dd
John Good <judiconis@gmail.com>
2016-10-23 22:08:50 +0100

    fixed listeners?

--- src/main/java/com/phx/reapercraft/ReaperCraftPlugin.java
+++ src/main/java/com/phx/reapercraft/ReaperCraftPlugin.java
@@ -3,13 +3,16 @@ package com.phx.reapercraft;
 import org.spongepowered.api.plugin.Plugin;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.event.entity.living.humanoid.player.RespawnPlayerEvent;
+import org.spongepowered.api.event.network.ClientConnectionEvent;
 import org.spongepowered.api.text.Text;
 import org.spongepowered.api.text.channel.MessageChannel;
+import org.spongepowered.api.event.Listener;
 
 @Plugin(id = "reapercraft", name = "ReaperCraft", version = "1.0")
 public class ReaperCraftPlugin {
 	
-	public void onSomeEvent(RespawnPlayerEvent event) {		
+	@Listener
+	public void onRespawnPlayer(RespawnPlayerEvent event) {		
         Player player = event.getTargetEntity();
         player.getInventory().clear();
         
@@ -17,5 +20,12 @@ public class ReaperCraftPlugin {
         MessageChannel broadcastChannel = MessageChannel.TO_ALL;
         broadcastChannel.send(this, text);
     }
+	
+	@Listener
+	public void onConnect(ClientConnectionEvent.Join event) {
+		Text text = Text.of("Reaper: Someone has connected!");
+        MessageChannel broadcastChannel = MessageChannel.TO_ALL;
+        broadcastChannel.send(this, text);
+	}
 
 }
