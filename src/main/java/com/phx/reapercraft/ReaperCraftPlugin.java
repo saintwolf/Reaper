f8a0aa98d2fa2db77d2f3def255903b3898548f2
John Good <judiconis@gmail.com>
2016-10-23 21:51:29 +0100

    Fixed broadcast?

--- src/main/java/com/phx/reapercraft/ReaperCraftPlugin.java
+++ src/main/java/com/phx/reapercraft/ReaperCraftPlugin.java
@@ -1,22 +1,21 @@
 package com.phx.reapercraft;
 
 import org.spongepowered.api.plugin.Plugin;
-import org.spongepowered.api.Server;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.event.entity.living.humanoid.player.RespawnPlayerEvent;
 import org.spongepowered.api.text.Text;
+import org.spongepowered.api.text.channel.MessageChannel;
 
 @Plugin(id = "reapercraft", name = "ReaperCraft", version = "1.0")
 public class ReaperCraftPlugin {
-	
-	private Server server;
 	
 	public void onSomeEvent(RespawnPlayerEvent event) {		
         Player player = event.getTargetEntity();
         player.getInventory().clear();
         
         Text text = Text.of("Reaper: Someone has died!");
-        server.getBroadcastChannel().send(text);
+        MessageChannel broadcastChannel = MessageChannel.TO_ALL;
+        broadcastChannel.send(this, text);
     }
 
 }
